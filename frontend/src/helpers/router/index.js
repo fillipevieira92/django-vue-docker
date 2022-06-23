7a86ef1b520f7f5ec79362be749c9d5be7e3cf47
fillipevieira92 <fillipevieira92@gmail.com>
2022-06-23 00:27:07 -0300

    redirect '/login'

--- frontend/src/helpers/router/index.js
+++ frontend/src/helpers/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', name: 'home', component: () => import(/* webpackChunkName: "home" */ '@/modules/common/pages/HomeView.vue') },
-  { path: '/about', name: 'about', component: () => import(/* webpackChunkName: "about" */ '@/modules/common/pages/AboutView.vue') },
+  { path: '/', redirect: "/login", name: 'home', component: () => import(/* webpackChunkName: "home" */ '@/modules/common/pages/HomeView.vue') },
+  { path: '/about', redirect: "/login", name: 'about', component: () => import(/* webpackChunkName: "about" */ '@/modules/common/pages/AboutView.vue') },
   { path: '/login', name: 'login', component: () => import(/* webpackChunkName: "login" */ '@/modules/auth/pages/LoginPage.vue') },
   { path: '/register', name: 'register', component: () => import(/* webpackChunkName: "login" */ '@/modules/auth/pages/RegisterPage.vue')},
 ]
